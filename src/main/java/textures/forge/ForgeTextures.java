2586749a99f361958f94cc32818241af6f79eaea
goldbattle <patfgeneva@gmail.com>
2014-06-14 18:28:22 -0400

    Added - Mod core class

---
+++ src/main/java/textures/forge/ForgeTextures.java
@@ -0,0 +1,18 @@
+package textures.forge;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ForgeTextures.MODID, version = ForgeTextures.VERSION)
+public class ForgeTextures {
+  
+  // All our static data
+  public static final String MODID = "Forge Textures";
+  public static final String VERSION = "alpha-0.1";
+
+  @EventHandler
+  public void init(FMLInitializationEvent event) {
+    
+  }
+}
